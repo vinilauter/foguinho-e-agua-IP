22f18ab2ba050dabf8f36921d9fd095d4e70cc27
vinilauter <vinnielauter@gmail.com>
2025-08-05 14:06:37 -0300

    "feat: Gravidade mais realista"

--- gravidade.py
+++ gravidade.py
@@ -1,10 +1,8 @@
-gravidade=0.8
+gravidade=0
 
 def aplicar_gravidade():
 
-    objeto.velocidade_y+=gravidade
-    objeto.rect.y+=objeto.velocidade_y
+    gravidade += 1
+    jogador_rect.y += gravidade
 
-    if objeto.rect.bottom>=altura_do_chao:
-        objeto.rect.bottom=altura_do_chao
-        objeto.velocidade_y=0
+    
