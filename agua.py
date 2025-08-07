105a9a7e568f8b6ceb34fd955403a0f4485f65e8
Caíque Oliveira <caiquemsdo@gmail.com>
2025-08-07 02:14:21 -0300

    Update agua.py

--- agua.py
+++ agua.py
@@ -5,6 +5,5 @@ class Agua(Jogador):
     def __init__(self, posicao_x, posicao_y, controles):
         cor = (50, 100, 255)  # Azul 
         super().__init__(posicao_x, posicao_y, cor, controles)
-        self.image = pygame.image.load("agua.png").convert_alpha()
+        self.image = pygame.image.load("watergirl.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, (40, 60))
-        
