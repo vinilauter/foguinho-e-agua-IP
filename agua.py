ba26f58f4982ea4526d1144fb0ccbf4000e30c7b
Caíque Oliveira <caiquemsdo@gmail.com>
2025-08-07 01:22:16 -0300

    Add files via upload

---
+++ agua.py
@@ -0,0 +1,8 @@
+import pygame
+from jogador import Jogador
+
+class Agua(Jogador):
+    def __init__(self, posicao_x, posicao_y, controles):
+        super().__init__(posicao_x, posicao_y, controles)
+        self.image = pygame.image.load("").convert_alpha() # Definir sprite
+        self.image = pygame.transform.scale(self.image, (,)) # Definir tamanho do personagem
