dc9a7bc3407b6243c92f9d9a38f2816b860ae6ad
Caíque Oliveira <caiquemsdo@gmail.com>
2025-08-07 01:20:55 -0300

    Update jogador.py

--- jogador.py
+++ jogador.py
@@ -1,15 +1,13 @@
 import pygame
+from foguinho import Foguinho
+from agua import Agua
 
-pygame.init()
-tela = pygame.display.set_mode((largura, altura)) # Tamanho da tela não definido | Criador do nível...
-
-# Classe jogador
 class Jogador(pygame.sprite.Sprite):
-    def __init__(self, posição_lateral_inicial, posicao_vertical_inicial, cor, controles): # Posição de inicio | Criador do nível...
+    def __init__(self, posicao_x, posicao_y, controles):
         super().__init__()
-        self.image = pygame.Surface((40, 60))  # Tamanho padrão do personagem | Só um exemplo, a gnt discute
-        self.image.fill(cor)
-        self.rect = self.image.get_rect()  # Posição inicial não definida | Criador do nível...
+        self.image = pygame.Surface((40, 60))  # Substituído pelas imagens nas subclasses
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (posicao_x, posicao_y)
 
         self.controles = controles
         self.vel_x = 0
@@ -17,21 +15,17 @@ class Jogador(pygame.sprite.Sprite):
 
     def update(self, teclas):
         self.vel_x = 0
-
         if teclas[self.controles['esquerda']]:
             self.vel_x = -5
         if teclas[self.controles['direita']]:
             self.vel_x = 5
-
-        # Movimento lateral
         self.rect.x += self.vel_x
 
-
-# Controles
+# Controles dos personagens
 controles_fogo = {
     'pular': pygame.K_UP,
     'esquerda': pygame.K_LEFT,
-    'direita': pygame.K_RIGHT
+    'direita': pygame.K_RIGHT   
 }
 
 controles_agua = {
@@ -40,14 +34,10 @@ controles_agua = {
     'direita': pygame.K_d
 }
 
-# Criação dos personagens
-fogo = Jogador(posicao_lateral_inicial, posicao_vertical_inicial, cor, controles_fogo) # Posição inicial e cor a definir
-agua = Jogador(posicao_lateral_inicial, posicao_vertical_inicial, cor, controles_agua) # Posição inicial e cor a definir
+# Jogadores
+foguinho = Foguinho(posicao_x, posicao_y, controles_fogo)
+agua = Agua(posicao_x, posicao_y, controles_agua)
 
-# Posição inicial não definida | Criador do nível...
-# fogo.rect.topleft = (100, 300)
-# agua.rect.topleft = (200, 300)
-
-# Grupo de sprites
 jogadores = pygame.sprite.Group()
-jogadores.add(fogo, agua)
+jogadores.add(foguinho)
+jogadores.add(agua)
