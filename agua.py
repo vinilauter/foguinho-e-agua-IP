719103a2d713ca4a9cfb8867977883a76465c31d
Caíque Oliveira <caiquemsdo@gmail.com>
2025-08-07 02:01:54 -0300

    Update agua.py

--- agua.py
+++ agua.py
@@ -3,6 +3,8 @@ from jogador import Jogador
 
 class Agua(Jogador):
     def __init__(self, posicao_x, posicao_y, controles):
-        super().__init__(posicao_x, posicao_y, controles)
-        self.image = pygame.image.load("").convert_alpha() # Definir sprite
-        self.image = pygame.transform.scale(self.image, (40, 60)) 
+        cor = (50, 100, 255)  # Azul 
+        super().__init__(posicao_x, posicao_y, cor, controles)
+        self.image = pygame.image.load("agua.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (40, 60))
+        
