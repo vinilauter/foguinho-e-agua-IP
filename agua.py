387bde050d47a641c6c28c0ff3786c56565cd00f
Caíque Oliveira <caiquemsdo@gmail.com>
2025-08-07 01:34:08 -0300

    Update agua.py

--- agua.py
+++ agua.py
@@ -5,4 +5,4 @@ class Agua(Jogador):
     def __init__(self, posicao_x, posicao_y, controles):
         super().__init__(posicao_x, posicao_y, controles)
         self.image = pygame.image.load("").convert_alpha() # Definir sprite
-        self.image = pygame.transform.scale(self.image, (,)) # Definir tamanho do personagem
+        self.image = pygame.transform.scale(self.image, (40, 60)) 
