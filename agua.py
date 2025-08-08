3a81051b43855a9ddea03d83b259dd702236265b
robsonlcj <rlandimcjr@gmail.com>
2025-08-08 15:25:57 -0300

    correcao dos diretorios

--- agua.py
+++ agua.py
@@ -5,6 +5,6 @@ class Agua(Jogador):
     def __init__(self, posicao_x, posicao_y, controles):
         cor = (50, 100, 255)  # Azul 
         super().__init__(posicao_x, posicao_y, cor, controles)
-        self.image = pygame.image.load("watergirl.png").convert_alpha()
+        self.image = pygame.image.load("Imagens/watergirl.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, (40, 60))
         self.retangulo = self.image.get_rect(topleft=(posicao_x, posicao_y))
--- foguinho.py
+++ foguinho.py
@@ -5,6 +5,6 @@ class Foguinho(Jogador):
     def __init__(self, posicao_x, posicao_y, controles):
         cor = (200, 50, 50)  # Vermelho
         super().__init__(posicao_x, posicao_y, cor, controles)
-        self.image = pygame.image.load("fireboy.png").convert_alpha()
+        self.image = pygame.image.load("Imagens/fireboy.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, (40, 60))
         self.retangulo = self.image.get_rect(topleft=(posicao_x, posicao_y))
