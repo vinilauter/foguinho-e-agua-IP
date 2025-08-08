6fcaed49f56d3375337a8366eee93829a2bcd825
robsonlcj <rlandimcjr@gmail.com>
2025-08-08 12:51:45 -0300

    correção diretório imagens

--- alavanca.py
+++ alavanca.py
@@ -5,8 +5,8 @@ class Alavanca(pygame.sprite.Sprite):
     def __init__(self, posicao, cor, ativada=False):
         super().__init__()
 
-        caminho_ligada = f"alavanca_{cor}_ON.png"
-        caminho_desligada = f"alavanca_{cor}_OFF.png"
+        caminho_ligada = f"Imagens/alavanca_{cor}_ON.png"
+        caminho_desligada = f"Imagens/alavanca_{cor}_OFF.png"
 
         self.img_ligada = pygame.image.load(caminho_ligada).convert_alpha()
         self.img_desligada = pygame.image.load(caminho_desligada).convert_alpha()
