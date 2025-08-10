765d4bd2ec7c7cc747d03acfde750e3aa187aee0
Caíque Oliveira <caiquemsdo@gmail.com>
2025-08-10 17:32:53 -0300

    Update agua.py aumento tamanho do personagem

--- agua.py
+++ agua.py
@@ -6,5 +6,5 @@ class Agua(Jogador):
         cor = (50, 100, 255)
         super().__init__(pos_x, pos_y, cor, controles)
         self.image = pygame.image.load("Imagens/watergirl.png").convert_alpha()
-        self.image = pygame.transform.scale(self.image, (40, 60))
+        self.image = pygame.transform.scale(self.image, (80, 70))
         self.rect = self.image.get_rect(topleft=(pos_x, pos_y))
